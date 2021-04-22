4cb0c17a0d7f70e663440985fb80f2de98afe7b3
jmendozacr <mendoza077@gmail.com>
2021-04-21 18:41:48 -0600

    adding function to add product

--- src/reducers/shoppingReducer.js
+++ src/reducers/shoppingReducer.js
@@ -8,8 +8,30 @@ const initialState = {
     cart: []
 }
 
-const reducer = (state = initialState, dispatch) => {
-    return state;
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD_PRODUCT":
+            const { id, name } = action.payload;
+		    const exist = state.cart.find((item) => item.id === id);
+
+            if(exist) {
+                let productCart = state.cart.map(item =>
+                    item.id === id ? {...exist, quantity: exist.quantity + 1} : item
+                );
+
+                return {
+                    ...state,
+                    cart: productCart
+                }
+            } else {
+                return {
+                    ...state,
+                    cart: [...state.cart, {id: id, name: name, quantity: 1}]
+                }
+            }
+        default:
+            return state;
+    }
 }
 
 export default reducer;
